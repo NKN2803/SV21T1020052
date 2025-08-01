e9b154db01633ce7787bd42e8e8d1ff680b8be6e
Ngô Kim Nguyên - 21T1020052 <106721658+NKN2803@users.noreply.github.com>
2025-08-01 14:52:49 +0700

    Add files via upload

---
+++ SV21T1020052.DomainModels/UserAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace SV21T1020052.DomainModels
+{
+    public class UserAccount
+    {
+        public string UserId { get; set; } = "";
+        public string UserName { get; set; } = "";
+        public string DisplayName { get; set; } = "";
+        public string Photo { get; set; } = "";
+        public string RoleNames { get; set; } = "";
+
+        // Các thuộc tính thêm cho đăng ký
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string Password { get; set; } = "";  // Chứa mật khẩu của người dùng
+    }
+}
